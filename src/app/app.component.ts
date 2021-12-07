132153fce3d449af90a6918c9fbcfc9118c45119
edivan <edivanssdf@gmail.com>
2021-12-07 19:42:24 -0300

    projeto web

---
+++ src/app/app.component.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+
+export class Cliente{
+  codigo?: number;
+  nome?: string;
+  cpf?: string;
+  telefone?: string;
+}
+
+export class Veiculo{
+  codigo?: number;
+  marca?: string;
+  modelo?: string;
+  valor?: number;
+  placa?: string;
+}
+
+export class Venda{
+  cliente?: Cliente;
+  veiculo?: Veiculo;
+  valorVenda?: number;
+}
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'concessionaria';
+
+  cliente: Cliente;
+  veiculo: Veiculo;
+  venda: Venda;
+  controleTela: string = 'cliente'
+
+  clientes: Cliente[] = [];
+  veiculos: Veiculo[] = [];
+  vendas: Venda[] = [];
+
+  constructor(){
+    this.cliente = new Cliente();
+    this.veiculo = new Veiculo();
+    this.venda = new Venda();
+  }
+
+
+
+  cadastrarCliente(){
+    this.clientes.push(this.cliente);
+    this.cliente = new Cliente();
+    return false;
+  }
+
+  cadastrarVeiculo(){
+
+  }
+
+  venderVeiculo(){
+
+  }
+
+
+  ativarTela(tela: string){
+    switch(tela){
+      case 'cliente':
+        this.controleTela = 'cliente';
+        break;
+      case 'veiculo':
+        this.controleTela = 'veiculo';
+        break;
+      case 'venda':
+        this.controleTela = 'venda';
+        break;       
+
+    }
+  }
+
+}
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
